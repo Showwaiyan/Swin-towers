08880a54b888d2c8cd9a36b964921bf090f703ab
Show Wai Yan <showwaiyan555@gmail.com>
2024-10-28 18:49:39 +0800

    Changed name variable to species variable

--- enemy.rb
+++ enemy.rb
@@ -1,14 +1,14 @@
 require 'gosu'
 
 class Enemy
-  attr_accessor :name, :hp, :speed, :path
+  attr_accessor :species, :hp, :speed, :path
   def generate_random_path
     # passed
     return path
   end
 
-  def initialize(name, hp, speed, path)
-    @name = name 
+  def initialize(species, hp, speed, path)
+    @species = species
     @hp = hp
     @speed = speed
     @path = path # path would be choosen randomly by give path
