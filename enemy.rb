a7491b5220df552b886649203bd0e880c5b2cac4
Show Wai Yan <showwaiyan555@gmail.com>
2024-10-28 21:46:37 +0800

    Finished Enemy path finding

--- enemy.rb
+++ enemy.rb
@@ -1,16 +1,45 @@
 require 'gosu'
+require 'matrix'
+require_relative 'constant.rb'
 
 class Enemy
-  attr_accessor :species, :hp, :speed, :path
-  def generate_random_path
-    # passed
-    return path
-  end
+  attr_accessor :obj, :pos, :hp, :speed, :path, :current_frame
 
-  def initialize(species, hp, speed, path)
-    @species = species
+  def initialize(obj, hp, speed)
+    @obj = obj
     @hp = hp
     @speed = speed
-    @path = path # path would be choosen randomly by give path
+    @path = PATHS.sample # Generating random path
+    @pos = Vector.elements(@path.shift) # pos is a 2D vector
+    @current_frame = 0
+  end
+
+  def move
+    return if @path.empty? # return if the path array is empty
+    # Calculate the direction to the next target
+    target = Vector.elements(@path.first)
+    direction = target - @pos
+    # normalize the direction
+    direction = direction.normalize * @speed
+    # move the enemy
+    @pos += direction
+
+    # draw the enemy
+    @obj[@current_frame].draw(center_x(@pos[0]), center_y(@pos[1]), ZOrder::CHAR)
+
+    # Checking if the enemy has reached the target
+    if (@pos - target).magnitude < @speed
+      @pos = target # set the position to the target because the enemy has reached the target
+      @path.shift # remove the previous target from the path
+      target = Vector.elements(@path.first) if @path.any? # this check whether the path array is empty
+    end
+  end
+
+  # Center the enemy image
+  def center_x(x)
+    x - (@obj[current_frame].width / 2)
+  end
+  def center_y(y)
+    y - (@obj[current_frame].height / 2)
   end
 end
--- main.rb
+++ main.rb
@@ -1,7 +1,7 @@
 require 'gosu'
-
-SCREEN_WIDTH = 1024
-SCREEN_HEIGHT = 768
+require 'matrix'
+require_relative 'constant.rb'
+require_relative 'enemy.rb'
 
 module ZOrder
   BACKGROUND, OBJECT, CHAR, UI = *0..3
@@ -11,13 +11,16 @@ class Game < Gosu::Window
   def initialize
     super(SCREEN_WIDTH, SCREEN_HEIGHT)
     self.caption = 'Swin Towers'
-    @bg = Gosu::Image.new('Assets/map_assets/map.png')
+    @bg = Gosu::Image.new('Assets/map/map.png')
+    @path = []
+    @enemies = []
   end
   
   def button_down(id)
     case id
-      when  Gosu::KB_ESCAPE
+      when  Gosu::KB_ESCAPE # Instantly close the game, just for prototype version
         close
+    end
   end
 
   def update
@@ -25,6 +28,7 @@ class Game < Gosu::Window
 
   def draw
     @bg.draw(0, 0, ZOrder::BACKGROUND)
+    @enemies.each { |enemy| enemy.move }
   end
 end
 
