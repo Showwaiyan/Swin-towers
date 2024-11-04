cf5d94fdd892e31dd0e117cc30da64d53c7f62b8
Show Wai Yan <showwaiyan555@gmail.com>
2024-11-04 09:19:54 +0800

    Finished upgrade button

--- ui.rb
+++ ui.rb
@@ -22,34 +22,13 @@ class UI
   end
 
   def draw_button
-    # draw_thick_line(@x, @y, @x + @width, @y, @border_color, 5)
-    # draw_thick_line(@x, @y + @height, @x + @width, @y + @height, @border_color, 5)
-    # draw_thick_line(@x, @y, @x, @y + @height, @border_color, 5)
-    # draw_thick_line(@x + @width, @y, @x + @width, @y + @height, @border_color, 5)
-
     # draw background
     @bg.draw(@x, @y, ZOrder::UI0)
-    # Gosu.draw_rect(@x, @y, @width, @height, @bg_color, ZOrder::UI0)
 
     # draw image
-    @img.draw(@x+(@width/2)-(@img.width/2), @y-(@img.height/2), ZOrder::UI1) if not @img.nil? && @img.height >= 130 # indicating that the image is tower
-  end
-
-  def draw_thick_line(x1, y1, x2, y2, color, width)
-    angle = Math.atan2(y2 - y1, x2 - x1)
-    offset_x = Math.sin(angle) * width / 2
-    offset_y = Math.cos(angle) * width / 2
-  
-    # Draw two lines slightly offset to each side
-    Gosu.draw_quad(
-      x1 - offset_x, y1 + offset_y, color,
-      x1 + offset_x, y1 - offset_y, color,
-      x2 + offset_x, y2 - offset_y, color,
-      x2 - offset_x, y2 + offset_y, color,
-      ZOrder::UI1
-    )
-  end
-  
+    @img.draw(@x+(@width/2)-(@img.width/2), @y-(@img.height/2), ZOrder::UI1) if @ui_type == TOWER_CREATE_BTN_UI_TYPE
+    @img.draw((@width-@img.width)+@x,(@height-@img.height)/2+@y, ZOrder::UI1) if @ui_type == TOWER_UPGRADE_BTN_UI_TYPE
+  end 
 
   def get_ui_type
     return @ui_type
